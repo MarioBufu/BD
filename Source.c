b487f7a0319235a54b9a3cac4f48e330bc60bab5
MarioBufu <30401027+MarioBufu@users.noreply.github.com>
2017-07-25 15:51:45 +0300

    Update Source.c
    +set 3,4

--- Source.c
+++ Source.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define PROB 7
+#define PROB 666
 
 #define PROB1 1 
 #define PROB2 2 
@@ -13,6 +13,15 @@
 #define PROB8 8
 #define PROB9 9
 
+#define PROB3_1 11
+#define PROB3_2 12
+#define PROB3_3 13
+#define PROB3_4 14
+#define PROB3_6 16
+
+#define PROB4_1 21
+
+#define PROB666 666
 
 #if(PROB == PROB1)
 #define PARAM1 9
@@ -72,6 +81,41 @@ int binSearch(int val, int vec[]);
 int search(int val, int vec[]);
 #endif /* (PROB == PROB9) */
 
+#if(PROB == PROB3_1)
+void printBytes(unsigned long *p);
+#endif /* (PROB == PROB3_1) */
+
+#if(PROB == PROB3_2)
+#define FLASH_BIUCR 432
+void flashRegSet(unsigned short *p, char APC13_15, char WWSC11_12, char RWSC8_10, char DPFEN6_7, char IPFEN4_5, char PFLIM1_3, char BFEN0);
+void flashRegPrint(unsigned short *p);
+#endif /* (PROB == PROB3_2) */
+
+#if(PROB == PROB3_3)
+#define LEN 6
+void calcul(char *ptablou, long *min, long *max, long *sum);
+#endif /* (PROB == PROB3_3) */
+
+#if(PROB == PROB3_4)
+void strncpy(char *destinatie, char *sursa, unsigned int n);
+#endif /* (PROB == PROB3_4) */
+
+#if(PROB == PROB3_6)
+#define N 4
+#define M 4
+void matSort(unsigned int mat[N][M]);
+void swap(unsigned int *a, unsigned int *b);
+#endif /* (PROB == PROB3_4) */
+
+#if(PROB == PROB4_1)
+void setReg(unsigned char *p);
+void printReg(unsigned char p);
+#endif /* (PROB == PROB4_1) */
+
+#if(PROB == PROB666)
+void retNrOf1Bits(unsigned char *p, unsigned long *count);
+#endif /* (PROB == PROB666) */
+
 int main()
 {
 	printf("\tHello\n\n");
@@ -99,7 +143,7 @@ int main()
 #endif /* (PROB == PROB7) */
 
 #if(PROB == PROB8)
-	printf("\n%d\n",nthFibTerm(25));
+	printf("\n%d\n",nthFibTerm(6));
 #endif /* (PROB == PROB8) */
 
 #if(PROB == PROB9)
@@ -109,6 +153,54 @@ int main()
 	printf("\n%d\n",binSearch(valCautata,vector));
 #endif /* SearchMethod */
 #endif /* (PROB == PROB9) */
+
+#if(PROB == PROB3_1)
+	unsigned long param = 34683;
+	printBytes(&param);
+#endif /* (PROB == PROB3_1) */
+
+#if(PROB == PROB3_2)
+	unsigned short p = FLASH_BIUCR;
+	flashRegSet(&p,7,3,7,3,3,7,1);
+	flashRegPrint(&p);
+#endif /* (PROB == PROB3_2) */
+
+#if(PROB == PROB3_3)
+	
+	char ptablou[LEN+1] = {LEN,3,9,5,3,7,1};
+	long min, max, sum;
+	calcul( ptablou,  &min,  &max,  &sum);
+	printf("\n min %ld\tmax %ld\tsum %ld\n",min,max,sum);
+#endif /* (PROB == PROB3_3) */
+
+#if(PROB == PROB3_4)
+	char d[] = "destinatie", s[] = "sursa";
+	int n = 3;
+	strncpy(d,s,n);
+	printf("\ndestinatie\t%s\nsursa\t\t%s",d,s);
+#endif /* (PROB == PROB3_4) */
+
+#if(PROB == PROB3_6)
+	 int mat[N][M] = { 
+		3, 1, 5, 2,
+		2, 5, 2, 6,
+		7, 2, 5, 2,
+		9, 1, 3, 6 };
+	matSort(mat);
+#endif /* (PROB == PROB3_4) */
+
+#if(PROB == PROB4_1)
+	unsigned char reg;
+	setReg(&reg);
+	printReg(reg);
+#endif /* (PROB == PROB4_1) */
+
+#if(PROB == PROB666)
+	unsigned char c = 10;
+	unsigned long count = 0;
+	retNrOf1Bits(&c, &count);
+	printf("\n%x -> %ld\n",c,count);
+#endif /* (PROB == PROB666) */
 	printf("\n");
 	return 0;
 }
@@ -327,3 +419,149 @@ int binSearch(int val, int vec[])
 }
 #endif /* SearchMethod */
 #endif /* (PROB == PROB9) */
+
+#if(PROB == PROB3_1)
+void printBytes(unsigned long *p)
+{
+	int i;
+	char *c;
+	c = &p;
+	for (i = 0; i < sizeof(*p); i++)
+	{
+		printf("%d ", c[i]);
+	}
+	printf("\n%ld",*p);
+}
+#endif /* (PROB == PROB3_1) */
+
+#if(PROB == PROB3_2)
+void flashRegSet(unsigned short *p, char APC13_15, char WWSC11_12, char RWSC8_10, char DPFEN6_7, char IPFEN4_5, char PFLIM1_3, char BFEN0)
+{
+	/* init bits recieved as param*/
+	*p = (APC13_15 << 13) + (WWSC11_12 << 11) + (RWSC8_10 << 8) + (DPFEN6_7 << 6) + (IPFEN4_5 << 4) + (PFLIM1_3 << 1) + BFEN0;
+}
+
+void flashRegPrint(unsigned short *p)
+{
+	printf("\n%x\n",*p);
+}
+#endif /* (PROB == PROB3_2) */
+
+#if(PROB == PROB3_3)
+void calcul(char *ptablou, long *min, long *max, long *sum)
+{
+	*min = *max = ptablou[1];
+	*sum = 0;
+	int i;
+	for (i = 1; i <= ptablou[0]; i++)
+	{
+		if ((long)ptablou[i] < *min)
+		{
+			*min = ptablou[i];
+		}
+		if ((long)ptablou[i] > *max)
+		{
+			*max = ptablou[i];
+		}
+		*sum += ptablou[i];
+	}
+}
+#endif /* (PROB == PROB3_3) */
+
+#if(PROB == PROB3_4)
+void strncpy(char *destinatie, char *sursa, unsigned int n)
+{
+	/* copiaza n caractere */
+	while (n > 0)
+	{
+		*destinatie = *sursa;
+		destinatie++;
+		sursa++;
+		n--;
+	}
+}
+#endif /* (PROB == PROB3_4) */
+
+#if(PROB == PROB3_6)
+void matSort(unsigned int mat[N][M])
+{
+	int swapped, i,j;
+	for (i = 0; i < N; i++)
+	{
+		for (j = 0; j < M; j++)
+		{
+			printf("%d ",mat[i][j]);
+		}
+		printf("\n");
+	}
+	for (j = 0; j < N; j++)
+	{
+		do {
+			swapped = 0;
+			for (i = 1; i < M; i++)
+			{
+				if (mat[j][i - 1] > mat[j][i]) {
+					swap(&mat[j][i-1],&mat[j][i]);
+					swapped = 1;
+				}
+			}
+		} while (swapped);
+	}
+	printf("\n\n");
+	for (i = 0; i < N; i++)
+	{
+		for (j = 0; j < M; j++)
+		{
+			printf("%d ", mat[i][j]);
+		}
+		printf("\n");
+	}
+}
+void swap(unsigned int *a,unsigned int *b)
+{
+		*a = *a + *b;
+		*b = *a - *b;
+		*a = *a - *b;
+}
+#endif /* (PROB == PROB3_6) */
+
+#if(PROB == PROB4_1)
+void setReg(unsigned char *p)
+{
+	union{
+		unsigned char all_bits;
+		struct{
+			unsigned char WPS0 : 1;
+			unsigned char WPE1 : 1;
+			unsigned char : 1;
+			unsigned char : 1;
+			unsigned char HYS4 : 1;
+			unsigned char ODE5 : 1;
+			unsigned char DSC6_7 : 2;
+			
+		}bitfield;
+	}SIU_PCR;
+	SIU_PCR.all_bits = 14;
+	SIU_PCR.bitfield.DSC6_7 = 2;
+	*p = SIU_PCR.all_bits;
+}
+void printReg(unsigned char p)
+{
+	printf("\n%x\n",p);
+}
+#endif /* (PROB == PROB4_1) */
+
+#if(PROB == PROB666)
+void retNrOf1Bits(unsigned char *p,unsigned long *count)
+{
+	unsigned char local = *p;
+	while (local)
+	{
+		if (local & 1)
+		{
+			(*count)++;
+		}
+		local = local >> 1;
+	}
+}
+#endif /* (PROB == PROB666) */
