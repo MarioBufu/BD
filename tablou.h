ec5c40c8a462eab793d7f97f145d32c65eb44c9d
MarioBufu <30401027+MarioBufu@users.noreply.github.com>
2020-03-09 22:14:31 +0200

    Delete tablou.h

--- tablou.h
+++
@@ -1,8 +0,0 @@
-#define LUNGIME_N 12
-#define LUNGIME_PP 10
-#define LUNGIME_NPP 10
-#define LUNGIME_NN 2
-int tab_numere[LUNGIME_N] = {-4,3,2,19,-453,-132,-57,-32,6,-3,64,-145};
-int tab_nrPrimePozitive[LUNGIME_PP];
-int tab_nrNonPrimePozitive[LUNGIME_NPP];
-int tab_nrNegative[LUNGIME_NN];
